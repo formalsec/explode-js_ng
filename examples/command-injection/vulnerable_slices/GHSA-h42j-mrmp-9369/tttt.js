0500a90c6476ed6762d71fc5d45321408a5fc934
Goncalo Carvalho <goncalocarvalhotic@gmail.com>
2025-04-17 11:28:21 +0100

    Replaced non exploitable example GHSA-h42j-mrmp-9369 with new example

--- examples/command-injection/vulnerable_slices/GHSA-h42j-mrmp-9369/tttt.js
+++
@@ -1,8 +0,0 @@
-const gitCommitInfo = require('git-commit-info')
-// information of the latest commit in ./my_repo
-gitCommitInfo({
-  cwd: '.',
-  commit: '82442c2405804d7aa44e7bedbc0b93bb17707626' + " || touch ci ||", // a malicious file named ci will be crated
-});
-
-
